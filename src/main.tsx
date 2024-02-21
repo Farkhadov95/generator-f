7128e461404e26d46bf78c3df3c66b7dfcfd4fc1
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-21 16:55:29 +0500

    Removed query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import theme from "./theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
